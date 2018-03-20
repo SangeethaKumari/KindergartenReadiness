8bf0ccb734568ca390138ba4902988316342b8e3
sangeetha_gsk <sangeethakumari.g@gmail.com>
2018-03-20 16:03:21 -0700

    Removing unneccessary methods

--- app/src/main/java/com/example/android/kindergarten/readiness/QuizActivity.java
+++ app/src/main/java/com/example/android/kindergarten/readiness/QuizActivity.java
@@ -1,12 +1,9 @@
 package com.example.android.kindergarten.readiness;
 
 import android.content.Intent;
-import android.os.PersistableBundle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
-import android.widget.Button;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.TextView;
